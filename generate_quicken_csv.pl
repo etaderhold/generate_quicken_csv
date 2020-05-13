3c97853af17c307afd0b04b7b9ca8883b62ffcd2
etaderhold <github@eric.aderhold.us>
2020-05-13 12:39:24 -0700

    Added support for Vanguard 529 funds.

--- generate_quicken_csv.pl
+++ generate_quicken_csv.pl
@@ -13,8 +13,7 @@ foreach my $investment (@$investments) {
 	if ($type eq 'ticker') {
 		my $ticker = $investment -> {'ticker'};
 		if ($ticker) {
-			my $description = $investment -> {'description'};
-			$description = $ticker unless $description;
+			my $description = $investment -> {'description'} || $ticker;
 			get_yahoo_csv($ticker, $description);
 		}
 		else {
@@ -23,34 +22,39 @@ foreach my $investment (@$investments) {
 	} elsif ($type eq 'vanguard_trust') {
 		my $id = $investment -> {'id'};
 		if ($id =~ /^\d{4}$/) {
-			my $description = $investment -> {'description'};
-			$description = $id unless $description;
+			my $description = $investment -> {'description'} || $id;
 			get_vanguard_trust_csv($id, $description);
 		}
 		else {
 			print "Type 'vanguard_trust' requires a four-digit ID. Skipping...\n";
 		}
-
+	} elsif ($type eq 'vanguard_529') {
+		my $id = $investment -> {'id'};
+		if ($id =~ /^\d{4}$/) {
+			my $description = $investment -> {'description'} || $id;
+			get_vanguard_529_csv($id, $description);
+		}
+		else {
+			print "Type 'vanguard_529' requires a four-digit ID. Skipping...\n";
+		}
 	}
 }
 
 sub get_vanguard_trust_csv {
 	my $id = shift;
 	my $description = shift;
-	$description =~ s/\s//g;
 	
 	my $json_destination = "$output_dir/$id.json";
 	my $csv_destination = "$output_dir/$description.csv";
 
 	# Download JSON history for the fund from an undocumented Vanguard API.
 	my $url = "https://api.vanguard.com/rs/ire/01/pe/fund/$id/price/price-history/.json?range=1Y";
-	print "Downloading history for Vanguard fund ID $id...";
+	print "Downloading history for Vanguard trust fund $description...";
 	`wget --referer=https://investor.vanguard.com/ --no-check-certificate $url -O $json_destination 2>&1`;
 	print "Done\n";
 
 	# Convert JSON to CSV
 	print "Converting JSON for Vanguard fund ID $id to CSV...";
-	my $description = $ids{$id};
 	open CSV, '>', $csv_destination or die "Could not open file for writing.";
 	print CSV "Date,Close,High,Low,Volume,Open\n";
 	my $ref = json_file_to_perl($json_destination);
@@ -69,13 +73,11 @@ sub get_vanguard_trust_csv {
 sub get_yahoo_csv {
 	my $ticker = shift;
 	my $description = shift;
-	$description =~ s/\s//g;
 
 	my $current_time = time();
-	my $year_ago = $current_time - 60 * 60 * 24 * 366;
+	my $year_ago = $current_time - 60 * 60 * 24 * 365;
 
 	my $url = "https://query1.finance.yahoo.com/v7/finance/download/$ticker?period1=$year_ago&period2=$current_time&interval=1d&events=history";
-	print "$url\n";
 	my $destination = "$output_dir/$description.csv";
 	my $destination_tmp = "$destination.tmp";
 	my $user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36";
@@ -89,3 +91,48 @@ sub get_yahoo_csv {
 	`mv $destination_tmp $destination`;
 }
 
+sub get_vanguard_529_csv {
+	my $id = shift;
+	my $description = shift;
+
+	my $html_destination = "$output_dir/$id.html";
+	my $csv_destination = "$output_dir/$description.csv";
+
+	# Download HTML price history from Vanguard
+	my $current_time = time();
+	my $end = get_url_date_string($current_time);
+
+	my $year_ago = $current_time - 60 * 60 * 24 * 365;
+	my $start = get_url_date_string($year_ago);
+
+	my $url = "https://personal.vanguard.com/us/funds/tools/pricehistorysearch?radio=1&results=get&FundType=529Plans&FundIntExt=INT&FundId=$id&fundName=$id&radiobutton2=1&beginDate=$start&endDate=$end&year=#res";
+	my $command = "wget --no-check-certificate \"$url\" -O $html_destination";
+	print "Downloading history for Vanguard 529 fund $description...";
+	`$command 2>&1`;
+	print "...done\n";
+
+	# Scrape HTML, output to CSV
+	print "Converting HTML for $description to CSV...";
+	open $csv, '>', $csv_destination or die "Could not open file for writing.";
+	print $csv "Date,Close,High,Low,Volume,Open\n";
+	open (my $html, $html_destination) or die "Could not open HTML!";
+	while (<$html>) {
+		if (/<td align="left">(.*)<\/td><td class="nr">\$(.*)<\/td>/) {
+			my $date = $1;
+			my $price = $2;
+			print $csv "$date,$price,0,0,0,0\n";
+		}
+	}
+	close $csv;
+
+	`rm $html_destination`;
+	print "...Done\n";
+}
+
+sub get_url_date_string {
+	my $time = shift;
+	my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime($time);
+	$mon++;
+	$year += 1900;
+	return "$mon%2F$mday%2F$year";
+}
