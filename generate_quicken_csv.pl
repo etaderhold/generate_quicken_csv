02c6826268bea62d0464b507c7363d1326825ce2
Eric Aderhold <eric@aderhold.us>
2021-03-02 12:38:54 -0800

    Vanguard DAF API change.

--- generate_quicken_csv.pl
+++ generate_quicken_csv.pl
@@ -60,7 +60,7 @@ sub get_vanguard_trust_csv {
 	my $id = shift;
 	my $description = shift;
 	$description =~ s/\s//g;
-	
+
 	my $json_destination = "$output_dir/$id.json";
 	my $csv_destination = "$output_dir/$description.csv";
 
@@ -92,25 +92,17 @@ sub get_vanguard_daf_csv {
 	my $description = shift;
 	$description =~ s/\s//g;
 
-	my $token_destination = "$output_dir/$id.token.json";
 	my $history_json_destination = "$output_dir/$id.history.json";
 	my $csv_destination = "$output_dir/$description.csv";
 
 	print "Downloading history for Vanguard DAF fund $description...";
-	# Get access token
-	my $header = 'Authorization: Basic ZG9ub3JQb3J0YWxVSTpzZWNyZXQ=';
-	my $post_data = 'grant_type=password&client_id=donorPortalUI&client_secret=secret&username=pwuser&password=Tomy%401234';
-	my $token_url = 'https://www.vanguardcharitable.org/donor-portal/api/oauth/token';
-	`wget --header='$header' --post-data='$post_data' $token_url -O $token_destination 2>&1`;
-	my $token = json_file_to_perl($token_destination)->{'access_token'};
-	print "auth token done...";
 
 	# Get price history
-	$header = "Authorization: Bearer $token";
 	my $start_date = get_csv_date_string(time - 60 * 60 * 24 * 365);
 	my $end_date = get_csv_date_string(time);
-	my $history_url = "https://www.vanguardcharitable.org/donor-portal/api/investmentOptions/findHistoricalNAVByPoolIdsAndStartDateAndEndDate?firstPoolId=$id&secondPoolId=11&startDateStr=$start_date&endDateStr=$end_date&isIOD=true";
-	`wget --header='$header' --no-check-certificate '$history_url' -O $history_json_destination 2>&1`;
+	my $history_url = "https://www.vanguardcharitable.org/donor-portal/api/pwuser/findHistoricalNAVByPoolIdsAndStartDateAndEndDate?firstPoolId=$id&secondPoolId=11&startDateStr=$start_date&endDateStr=$end_date&isIOD=true";
+
+	`wget --no-check-certificate '$history_url' -O $history_json_destination 2>&1`;
 	print "price history done.\n";
 
 	print "Converting DAF history to CSV...";
@@ -125,7 +117,6 @@ sub get_vanguard_daf_csv {
 		print $csv "$date,$price,0,0,0,0\n";
 	}
 	close $csv;
-	`rm $token_destination`;
 	`rm $history_json_destination`;
 	print "done\n";
 }
